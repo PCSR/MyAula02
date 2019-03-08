598d9a401e0885a10d550550c556e37ce0e53efd
PCSR <pedrocsr_5@hotmail.com>
2019-03-08 12:43:41 +0000

    Volume e area do cilindro (Math.Pi e Math.Pow)com comentarios

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,19 +6,26 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            float Volume;
-            float Area;
+            double Volume;
+            double Area;
+            double Volume2;
             float pi = 3.1415926f;
             Console.WriteLine("Introduza o valor do raio: " );
             string raio = Console.ReadLine();
+            //Converte string recebida pelo utilizador num float
             float r = Convert.ToSingle(raio);
             Console.WriteLine("Introduza o valor da altura: ");
             string altura = Console.ReadLine();
+            //Converte string recebida pelo utilizador num float
             float a = Convert.ToSingle(altura);
             Volume = pi*r*r*a;
+            Console.WriteLine($"O Volume do cilindro é: {Volume:f3}");
             Area = 2 * pi * r * (r + a);
-            Console.WriteLine($"o Volume do cilindro é: {Volume:f3}");
-            Console.WriteLine("A area do cilindro é: {0:f3}", Area);
+            Console.WriteLine("A Area do cilindro é: {0:f3}", Area);
+            //Volume usando Math.Pow e Math.Pow em vez de pi e r*r
+            Volume2 = Math.PI * Math.Pow(r,2)*a;
+            Console.WriteLine($"O Volume do cilindro é: {Volume2:f3}");
+
         }
     }
 }
