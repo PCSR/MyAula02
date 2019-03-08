06adde4d2a3397920c37938343e3c2c8be2e6f0d
PCSR <pedrocsr_5@hotmail.com>
2019-03-08 10:23:01 +0000

    Tipos de strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -10,11 +10,18 @@ namespace VariasStrings
             string a = "\"String 1\"";
             string b = @"\String 2\";
             string c = @"""String 3""";
+            string d = "123" + a;
+            string e = $"{a} mais {c}";
+            string f= String.Format("a e' {0}, e b e' {1}", a, b);
 
 
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(@"Verbatim com a = {0}", a);
         }
     }
 }
